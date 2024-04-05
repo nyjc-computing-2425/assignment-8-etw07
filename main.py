79faec19f572be9bfa76ade98f943684740f3fba
Ethan W. <160505433+etw07@users.noreply.github.com>
2024-04-05 07:02:10 +0000

    final submission

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # Built-in imports
 
+def reverse(text):
+  """takes an string of text and returns it in reverse"""
+  if len(text) <= 1:
+    return text
+  return reverse(text[1:]) + text[0]
+
+def is_palindrome(string):
+  """Takes in a string and returns boolean True or False depending on if the string is a palindrome"""
+  string.strip()
+  if len(string) <= 1:
+    return True
+  elif string[0] != string[-1]:
+    return False
+  return is_palindrome(string[1:-1])
